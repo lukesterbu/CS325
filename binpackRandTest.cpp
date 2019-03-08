82d708deb70aba47378810b84ec71b861455ce25
Luke <41594454+lukesterbu@users.noreply.github.com>
2019-03-08 10:38:18 -0500

    Update binpackRandTest.cpp

--- binpackRandTest.cpp
+++ binpackRandTest.cpp
@@ -1,7 +1,3 @@
-﻿// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
